02a8a649304c7e2dba9fad8a782879a001a2ca9f
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2024-12-23 10:54:05 -0500

    NTS

--- resources/views/components/sales/table.blade.php
+++ resources/views/components/sales/table.blade.php
@@ -10,7 +10,7 @@
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" /> --}}
 
-<div id="gridContainer"></div>
+<div id="gridContainer" class="w-full"></div>
 
 <script>
   const salesDataGrid = $('#gridContainer').dxDataGrid({
